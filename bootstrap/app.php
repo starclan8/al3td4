f981709ecbdc91531a76d582fd50663f9343ec64
al3td <star.c1017@gmail.com>
2025-11-22 16:46:22 -0600

    Remove redundant CheckRole middleware
    - Remove CheckRole middleware (redundant with Spatie Permission)
    - Spatie already provides role:name middleware out of the box
    - Keep AdminMiddleware for backward compatibility
    - Simplify middleware configuration in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
-            'role' => \App\Http\Middleware\CheckRole::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
